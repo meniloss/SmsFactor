27fab4ad2d6eb62fb155a6d31aa81054b781794b
meniloss <50333010+meniloss@users.noreply.github.com>
2020-03-10 12:26:16 +0100

    Update MenilossSmsFactorExtension.php

--- DependencyInjection/MenilossSmsFactorExtension.php
+++ DependencyInjection/MenilossSmsFactorExtension.php
@@ -5,7 +5,7 @@ namespace Meniloss\SmsFactorBundle\DependencyInjection;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
-use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\Routing\Loader\XmlFileLoader;
 
 /**
  * This is the class that loads and manages your bundle configuration.
@@ -22,10 +22,11 @@ class MenilossSmsFactorExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
         
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.yml');
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.xml');
         
         $definition = $container->getDefinition('meniloss_sms_factor.request_manager');
         $definition->replaceArgument(0, $config['api_key']);
     }
 }
+
